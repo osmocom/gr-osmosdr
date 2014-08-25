7703aabf35016987eca828ffde6daf0e57cda678
Dimitri Stolnikov <horiz0n@gmx.net>
2014-08-25 22:33:12 +0200

    python: Remove an old workaround for a swig bug
    following gnuradio commit 39f14138ac9158eb0610e89a2ae1f8290f184c44
    
    This has been removed of Gnuradio iteself in 2012 and this seems to
    be causing issues for some OOT on some platforms...

--- python/__init__.py
+++ python/__init__.py
@@ -19,36 +19,11 @@
 # The presence of this file turns this directory into a Python package
 
 '''
-This is the GNU Radio OsmoSDR module. Place your Python package
-description here (python/__init__.py).
+This is the GNU Radio OsmoSDR module.
 '''
-
-# ----------------------------------------------------------------
-# Temporary workaround for ticket:181 (swig+python problem)
-import sys
-_RTLD_GLOBAL = 0
-try:
-    from dl import RTLD_GLOBAL as _RTLD_GLOBAL
-except ImportError:
-    try:
-        from DLFCN import RTLD_GLOBAL as _RTLD_GLOBAL
-    except ImportError:
-        pass
-
-if _RTLD_GLOBAL != 0:
-    _dlopenflags = sys.getdlopenflags()
-    sys.setdlopenflags(_dlopenflags|_RTLD_GLOBAL)
-# ----------------------------------------------------------------
-
 
 # import swig generated symbols into the osmosdr namespace
 from osmosdr_swig import *
 
 # import any pure python here
 #
-
-# ----------------------------------------------------------------
-# Tail of workaround
-if _RTLD_GLOBAL != 0:
-    sys.setdlopenflags(_dlopenflags)      # Restore original flags
-# ----------------------------------------------------------------
