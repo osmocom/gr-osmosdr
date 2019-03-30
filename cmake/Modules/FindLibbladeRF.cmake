ffc2126cbe2aba35a5e6064646773cb323541f79
Robert Ghilduta <robert.ghilduta@nuand.com>
2019-03-30 02:34:32 -0700

    bladeRF: check libbladeRF version before compiling

--- cmake/Modules/FindLibbladeRF.cmake
+++ cmake/Modules/FindLibbladeRF.cmake
@@ -1,5 +1,9 @@
 if(NOT LIBBLADERF_FOUND)
   pkg_check_modules (LIBBLADERF_PKG libbladeRF)
+  if (LIBBLADERF_PKG_FOUND AND LIBBLADERF_PKG_VERSION VERSION_LESS "2")
+    message( FATAL_ERROR "Install version 2 or greater of libbladeRF."
+      " Current version ( ${LIBBLADERF_PKG_VERSION} ) is out of date." )
+  endif()
   find_path(LIBBLADERF_INCLUDE_DIRS NAMES libbladeRF.h
     PATHS
     ${LIBBLADERF_PKG_INCLUDE_DIRS}
